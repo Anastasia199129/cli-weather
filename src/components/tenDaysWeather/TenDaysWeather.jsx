e4436237d094803c3619438e374fadc31cb89311
Anastasia199129 <Nastasia199126@gmail.com>
2021-11-14 22:55:33 +0200

    c

--- src/components/tenDaysWeather/TenDaysWeather.jsx
+++ src/components/tenDaysWeather/TenDaysWeather.jsx
@@ -24,7 +24,7 @@ export default function TenDaysWeather({ city }) {
         }
       })
       .catch(function (error) {
-        return setCities(null);
+        return console.log(error.message);
       });
   }, [city]);
 
